6f5a8a3dcb4436e96821cebb6be8485dd1e6cb5b
ImJoabli <joacodmz.25@gmail.com>
2026-05-17 21:40:40 -0300

    Add files via upload

---
+++ machete.py
@@ -0,0 +1,11 @@
+print("Hola, mundo")          # Muestra texto (cadena)
+print(42)                     # Muestra un número
+print("Mi edad es", 25)       # Muestra varios elementos separados por espacio
+
+nombre = input("como te llamas?")
+print("Hola", nombre)
+
+edad = 28                  # int (número entero)
+precio = 19.99             # float (número con decimales)
+nombre = "Carlos"          # str (texto/cadena)
+es_estudiante = True       # bool (verdadero o falso)
